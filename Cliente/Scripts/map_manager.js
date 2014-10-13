ef1e042458c6b1d888f8bad87690ff44d1c4dd4c
msn.roberto.ds <msn.roberto.ds@gmail.com>
2014-10-13 06:16:15 +0000

    Buscador en semi-funcionamiento...

--- Cliente/Scripts/map_manager.js
+++ Cliente/Scripts/map_manager.js
@@ -4,7 +4,7 @@ function crearMapa(id_div){
             options:{
               center:[-22.49156846196823, -64.75802349999992],
               zoom:2,
-              mapTypeId: google.maps.MapTypeId.SATELLITE,
+              mapTypeId: google.maps.MapTypeId.ROUTE,
               mapTypeControl: true,
               mapTypeControlOptions: {
                 style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
@@ -18,15 +18,11 @@ function crearMapa(id_div){
 	//return $("#"+id_div).gmap3("get");
 }
 
-function agregarMarcadores(id_div, clickEvent){
+function agregarMarcadores(id_div, listaMarcadores, clickEvent){
 
 	$("#"+id_div).gmap3({
 		marker:{
-            values:[
-              {latLng:[48.8620722, 2.352047], data:"Paris !"},
-              {address:"86000 Poitiers, France", data:"Poitiers : great city !"},
-              {address:"66000 Perpignan, France", data:"Perpignan ! <br> GO USAP !", options:{icon: "http://maps.google.com/mapfiles/marker_green.png"}}
-            ],
+            values: listaMarcadores,
             options:{
               draggable: false
             },
