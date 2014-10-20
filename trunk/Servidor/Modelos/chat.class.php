7e896b4047e71f0359135621eca6b9a86a2f032f
msn.roberto.ds@gmail.com <msn.roberto.ds@gmail.com@04a522b4-2f00-e489-278d-ee6134a8d7d4>
2014-10-20 03:18:58 +0000

    Chat ya acumula mensajes si estas deslogueado y muestra los nuevos que recibiste apenas te logueas.

--- trunk/Servidor/Modelos/chat.class.php
+++ trunk/Servidor/Modelos/chat.class.php
@@ -2,18 +2,24 @@
 	class Chat{
 		public function obtenerChat($id_usuario){
 			global $miBD;
-			$query = "select * from chat where (chat.to = ? AND recd = 0) order by id ASC";
+			$query = "select * from chat where (chat.toUser = ? AND recd = 0) order by id ASC";
 			return $miBD->ejecutar($query, array($id_usuario));
 		}
 		public function actualizaRecibido($id_usuario){
 			global $miBD;
-			$query = "update chat set recd = 1 where chat.to = ? and recd = 0";
+			$query = "update chat set recd = 1 where chat.toUser = ? and recd = 0";
 			return $miBD->ejecutarSimple($query, array($id_usuario));
 		}
 		public function enviarChat($from, $to, $menssage){
 			global $miBD;
-			$query = "insert into chat (chat.from,chat.to,message,sent) values (?, ?,?,NOW())";
+			$query = "insert into chat (chat.fromUser,chat.toUser,message,sent) values (?, ?,?,NOW())";
 			return $miBD->ejecutarSimple($query, array($from, $to, $menssage));
+		}
+		//-----------------------------------------------------------------------
+		public function obtenerCantidadMensajesSinLeer($usuario_destino){
+			global $miBD;
+			$query = "SELECT count(*) FROM chat WHERE chat.recd=0 AND chat.toUser like ?";
+			return $miBD->ejecutarSimple($query, array($usuario_destino));
 		}
 	}
 ?>
