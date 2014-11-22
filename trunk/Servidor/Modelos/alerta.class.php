dae331724b505d11827ce83783dfa097ba5f7974
msn.roberto.ds@gmail.com <msn.roberto.ds@gmail.com@04a522b4-2f00-e489-278d-ee6134a8d7d4>
2014-11-22 00:58:02 +0000

    alertas funcionando

--- trunk/Servidor/Modelos/alerta.class.php
+++ trunk/Servidor/Modelos/alerta.class.php
@@ -27,12 +27,13 @@
 		//-----------------------------------------------------------------------
 		//generar alertas
 		//-----------------------------------------------------------------------
-		public function enviarAlertasParaInteresados($id_nuevo_producto, $categoria_producto, $usuario){
+		public function enviarAlertasParaInteresados($id_nuevo_producto, $categoria_producto, $id_usuario, $url_usuario){
 			global $miBD;
+			// busco si mi producto nuevo es deseado por alguien...
 			$query =" Select distinct 
 						p.id_producto, 
 						p.id_usuario,
-						u.url_usuario
+						u.id_usuario
 						From deseado_etiqueta de
 						Inner join producto_deseado pd on (pd.id_producto_deseado = de.id_producto)
 						inner join producto p on (p.id_producto = pd.id_producto)
@@ -43,23 +44,54 @@
 												 Where id_producto = ?						 
 						)
 						And pd.id_categoria = ? 
-						and u.id_usuario <> ?";
-			$resultado = $miBD->ejecutar($query, array($id_nuevo_producto, $categoria_producto, $usuario ), true);
+						and u.id_usuario <> ?
+						and u.debaja = 0
+						and p.debaja = 0
+						";
+			$resultado = $miBD->ejecutar($query, array($id_nuevo_producto, $categoria_producto, $id_usuario ), true);
 			$cantidad = count ($resultado);
+			// ...y les aviso a cada uno de ellos.
 			if ($cantidad  > 0){
 				for($i=0; $i<$cantidad; $i++){
-					$query = "Insert into alerta (mensaje_alerta,id_usuario, id_producto) Values(?,?,?)";
-								
-					$mensaje = $resultado[$i]['url_usuario']." desea un producto que quizás podrías tener...".$resultado[$i]['id_producto'] ;
-					$miBD ->ejecutar($query, array( $mensaje, $usuario, $resultado[$i]['id_producto'] ));			
+					$query = "Insert into alerta (mensaje_alerta, id_usuario, id_producto) Values(?,?,?)";								
+					$mensaje = $url_usuario." posee un producto que quizás podría interesarte...";
+					$miBD->ejecutar($query, array( $mensaje, $resultado[$i]['id_usuario'], $id_nuevo_producto )); //promuevo mi producto al usuario interesado	
 				}
 			}
 		}
 		
 		//-----------------------------------------------------------------------
-		public function enviarmeAlertasDeMisIntereses($id_nuevo_producto){
-		
-		
+		public function enviarmeAlertasDeMisIntereses($id_nuevo_producto, $categoria_producto_deseado, $id_usuario){
+			global $miBD;
+			// busco si mi deseo nuevo es parecido a productos ofrecidos por otros usuarios...		
+			$query =" Select distinct 
+						p.id_producto, 
+						p.id_usuario,
+						u.url_usuario
+						From producto_etiqueta pe						
+						Inner join producto p on (p.id_producto = pe.id_producto)
+						Inner join usuario u on (u.id_usuario = p.id_usuario)
+						Where pe.id_etiqueta in(
+												 Select id_etiqueta
+												 From deseado_etiqueta de
+												 inner join producto_deseado pd on (pd.id_producto_deseado = de.id_producto) 
+												 Where pd.id_producto = ?						 
+						)
+						And p.id_categoria = ? 
+						and u.id_usuario <> ?
+						and u.debaja = 0
+						and p.debaja = 0
+						";
+			$resultado = $miBD->ejecutar($query, array($id_nuevo_producto, $categoria_producto_deseado, $id_usuario ), true);
+			$cantidad = count ($resultado);
+			// ...que me avise de esos productos.	
+			if ($cantidad  > 0){
+				for($i=0; $i<$cantidad; $i++){
+					$query = "Insert into alerta (mensaje_alerta, id_usuario, id_producto) Values(?,?,?)";
+					$mensaje = $resultado[$i]['url_usuario']." posee un producto que quizás podría interesarte...";
+					$miBD->ejecutar($query, array( $mensaje, $id_usuario, $resultado[$i]['id_producto'] )); //avisarme a mi de productos interesantes	
+				}
+			}		
 		}
 	}
 ?>
