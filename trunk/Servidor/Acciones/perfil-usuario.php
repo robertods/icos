d36b6c19983f407b18a3f464380056b79fb22bff
msn.roberto.ds@gmail.com <msn.roberto.ds@gmail.com@04a522b4-2f00-e489-278d-ee6134a8d7d4>
2014-11-25 00:41:43 +0000

    SE arreglan cosas

--- trunk/Servidor/Acciones/perfil-usuario.php
+++ trunk/Servidor/Acciones/perfil-usuario.php
@@ -9,11 +9,12 @@
 			$usuario = new Usuario();
 			$perfil = $usuario ->obtenerPerfilUsuario($dato);
 			$var['base_modificada'] = '<base href="../"/>';
+			$var['enlace_modificado'] = 'perfil-usuario/'.$dato;
 			
 			$var['id_usuario'] = $perfil[0]['id_usuario'];
 			$var['nombre'] = $perfil[0]['nombre_perfil'];
 			$var['prestigio'] = $perfil[0]['prestigio_perfil'];
-			
+			$var['url_usuario'] = $perfil[0]['url_usuario'];
 			
 			
 			
